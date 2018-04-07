c74826aec767ad0e58641d32538eb211f6cf160f
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-04-07 16:24:58 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,25 +2,17 @@ require 'pry'
 
 class CashRegister
   attr_accessor :total, :last_transaction, :discount, :items
-    #@@items = []
+
   def initialize(discount = 0)
-    #@total = total
     @total = 0
     @discount = discount
     @items = []
-    #@total
-  #  binding.pry
   end
-
-  # def discount
-  #   @employee_discount
-  # end
 
   def add_item(title,price, quantity = 1)
     @last_transaction = price * quantity
     @total = @last_transaction + @total
-    @total
-    #binding.pry
+  #  @total
     new_arr = Array.new(quantity,title)
     @items << new_arr
     @items.flatten!
@@ -32,19 +24,13 @@ class CashRegister
 
     else
       @total = @total -  (@discount * 10)
-    #binding.pry
      "After the discount, the total comes to $#{@total}."
    end
   end
 
-  # def items
-  #   @items
-  # end
-
   def void_last_transaction
     @total = @total - @last_transaction
-    @total
-    #binding.pry
+    #@total
   end
 
 end
